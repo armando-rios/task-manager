78a9544d68c19fd40992ecc87cd0f8e29198bc83
Armando Rios <armandorios.dev@gmail.com>
2025-02-27 16:49:59 -0800

    refactor: delete unutilized files

--- src/utils/storage.js
+++
@@ -1,8 +0,0 @@
-import { renderProjects } from "../views/renderProjects.js"
-
-export const createProject = (name, description) => {
-  const projects = JSON.parse(localStorage.getItem("projects")) || []
-  projects.push({ name, description, id: Date.now() })
-  localStorage.setItem("projects", JSON.stringify(projects))
-  renderProjects(projects)
-}
