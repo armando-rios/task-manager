d1b8d32e81136831aa286def8f77cec18648d7f5
Armando Rios <armandorios.dev@gmail.com>
2025-02-28 21:52:15 -0800

    feat: create Task component

---
+++ src/models/Task.js
@@ -0,0 +1,12 @@
+export default class Task {
+  constructor ({ title, description = "", status = "pending", projectId, priority = "medium", dueDate = null }) {
+    this.id = projectId
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.status = status
+    this.priority = priority
+    this.createdAt = new Date().toISOString()
+    this.updatedAt = new Date().toISOString()
+  }
+}
