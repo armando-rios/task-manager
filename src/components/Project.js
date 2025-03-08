0bc9ba2728a8ffda1595f69e1e716459c213e12d
Aramando Rios <armandorios.dev@gmail.com>
2025-03-07 17:49:07 -0800

    feat: add delete project button

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,16 +14,28 @@ export const Project = (project) => {
   const desc = document.createElement("p")
   desc.textContent = project.description
 
+  const buttonsContainer = document.createElement("div")
+  buttonsContainer.className = "flex gap-2"
+
   const createTaskBtn = document.createElement("button")
   createTaskBtn.className = "bg-theme-primary h-fit text-theme-surface-0 font-bold py-2 px-4 rounded"
   createTaskBtn.id = "create-task-btn"
   createTaskBtn.textContent = "Create Task"
 
+  const deleteProjectBtn = document.createElement("button")
+  deleteProjectBtn.className = "bg-theme-priority-high h-fit text-theme-surface-0 font-bold py-2 px-4 rounded"
+  deleteProjectBtn.id = "delete-project-btn"
+  deleteProjectBtn.textContent = "Delete Project"
+
   titleContainer.append(title)
   titleContainer.append(desc)
 
-  projectHeader.append(titleContainer)
-  projectHeader.append(createTaskBtn)
+  buttonsContainer.append(createTaskBtn)
+  buttonsContainer.append(deleteProjectBtn)
 
+  projectHeader.append(titleContainer)
+  projectHeader.append(buttonsContainer)
+
+  // Return the project header and tasks container
   return [projectHeader, projectTasksContainer]
 }
