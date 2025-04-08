dd0f2c97dd51f5bcfd98369b6130d28b9c56cbcd
Armando Rios <armandorios.dev@gmail.com>
2025-04-07 20:37:54 -0700

    feat: use renderFilteredTasks on filterController

--- src/controllers/filterController.js
+++ src/controllers/filterController.js
@@ -1,3 +1,5 @@
+import { renderFilteredTasks } from "../views/renderTasks.js";
+
 export const initFilterController = () => {
   const filtersContainer = document.querySelector('#filters')
 
@@ -14,11 +16,17 @@ export const initFilterController = () => {
       return
     }
 
+    const projectId = activeProject.id;
+    const filterType = filterButton.getAttribute("data-filter");
+
     document.querySelectorAll('#filters [data-filter]').forEach((btn) => {
       btn.classList.remove('bg-theme-surface-2')
     })
     filterButton.classList.add('bg-theme-surface-2')
+
+    renderFilteredTasks(projectId, filterType)
   }
 
   filtersContainer.addEventListener('click', handleFilterClick)
+
 }
