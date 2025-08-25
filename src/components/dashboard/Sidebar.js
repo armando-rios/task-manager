741b227237308723a99870ce975633ff4c5c3676
Armando Rios <armandorios.dev@gmail.com>
2025-08-25 08:21:22 -0700

    refactor: change height size and set hidden sidebar for default

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -4,7 +4,7 @@ export function Sidebar() {
   const sidebarContainer = cD({
     tagName: 'div',
     styles:
-      'w-76 bg-theme-surface-0 p-4 border-r border-theme-surface-3 sm:flex flex flex-col',
+      'hidden w-76 bg-theme-surface-0 p-4 border-r border-theme-surface-3 sm:flex flex flex-col',
   })
 
   return sidebarContainer
