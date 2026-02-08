2c5e4eda9cb9a944460a06d99ed7a4a1a13bfd2f
Armando Rios <armandorios.dev@gmail.com>
2026-02-08 15:46:53 -0800

    refactor: remove unused legacy controllers and models

--- src/models/Project.js
+++
@@ -1,9 +0,0 @@
-export default class Project {
-  constructor(name, description = '') {
-    this.id = Date.now().toString();
-    this.name = name;
-    this.description = description;
-    this.createdAt = new Date().toISOString();
-    this.updatedAt = new Date().toISOString();
-  }
-}
